6fe807cb8c4ded1fe8b5980afa8e54741a5b2176
Likit Preeyanon <preeyano@msu.edu>
2018-04-03 02:35:34 +0700

    changed open menu to import
    grid now can reload data without having to destroy the panel and close grid works as we expected now

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import wx
-from components.gridtable import DataGrid
+import pandas
+from components.gridtable import DataGrid, DataTable
 
 VERSION = "0.1"
+
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, title):
@@ -11,7 +13,7 @@ class MainFrame(wx.Frame):
         filemenu = wx.Menu()
         gridmenu = wx.Menu()
         menuAbout = helpmenu.Append(wx.ID_ABOUT, "&About", "About the program")
-        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", "Open a file")
+        menuImport = filemenu.Append(wx.ID_OPEN, "&Import", "Import data")
         menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
         menuCloseGrid = gridmenu.Append(wx.ID_CLOSE, "Close", "Close")
 
@@ -22,15 +24,13 @@ class MainFrame(wx.Frame):
         self.SetMenuBar(menuBar)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.Bind(wx.EVT_MENU, self.OnImport, menuImport)
         self.Bind(wx.EVT_MENU, self.OnCloseGrid, menuCloseGrid)
 
-        self.panel = wx.Panel(self, -1)
         self.Show(True)
 
-
     def OnAbout(self, e):
-        aboutMsg = ('Armour Version %s\n\n'
+        aboutMsg = ('Armor Version %s\n\n'
                     'Developed at the Faculty of Medical Technology,\n'
                     'Mahidol University, Thailand,\n'
                     'to facilitate microbiology lab data analytics.\n\n'
@@ -40,35 +40,38 @@ class MainFrame(wx.Frame):
         dlg.ShowModal()
         dlg.Destroy()
 
-    def OnOpen(self, e):
+    def OnImport(self, e):
         self.dirname = ''
         dlg = wx.FileDialog(self, 'Choose a file', self.dirname, '', '*.*', wx.FD_OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
-            f = open(os.path.join(self.dirname, self.filename), 'r')
-            f.close()
+            if self.filename.endswith('xlsx') or self.filename.endswith('xls'):
+                df = pandas.read_excel(os.path.join(self.dirname, self.filename))
+            elif self.filename.endswith('csv'):
+                df = pandas.read_csv(os.path.join(self.dirname, self.filename))
         dlg.Destroy()
-        if self.panel.IsShown():
-            self.panel.Destroy()
-            # self.Layout()
-            self.Fit()
 
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.dataPanel = wx.Panel(self, -1)
-        self.grid = DataGrid(self.dataPanel)
-        self.sizer.Add(self.grid, 1, wx.EXPAND|wx.ALL)
-        self.dataPanel.SetSizer(self.sizer)
-        self.sizer.Fit(self.dataPanel)
-        # self.Layout()  # Layout does not work like we would expect
-        self.Fit()       # Fit seems to suffice in this case
+        if not hasattr(self, 'dataPanel'):
+            self.sizer = wx.BoxSizer(wx.VERTICAL)
+            self.dataPanel = wx.Panel(self, -1)
+            self.grid = DataGrid(self.dataPanel, data=df)
+            self.sizer.Add(self.grid, 1, wx.EXPAND | wx.ALL)
+            self.dataPanel.SetSizer(self.sizer)
+            self.sizer.Fit(self.dataPanel)
+            # self.Layout()  # Layout does not work like we would expect
+        else:
+            self.dataPanel.Hide()
+            self.grid.SetTable(DataTable(df))  # update data model
+            self.dataPanel.Show()
+        self.Fit()  # Fit seems to suffice in this case
 
     def OnExit(self, e):
         self.Close(True)
 
     def OnCloseGrid(self, e):
         if self.dataPanel.IsShown():
-            self.dataPanel.Destroy()
+            self.dataPanel.Hide()
             self.Layout()
 
 
