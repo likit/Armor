65148eee049201a9de65f7ad00b78bdb7f0ad8d9
Likit Preeyanon <preeyano@msu.edu>
2018-05-10 23:56:25 +0700

    added a dialog for schema setting
    edited code to handle the header better
    
    updated menus

--- app.py
+++ app.py
@@ -1,9 +1,49 @@
 import os
+import json
 import wx
 import pandas as pd
+from collections import defaultdict
 from components.gridtable import DataGrid, DataTable
 
 VERSION = "0.1"
+
+
+class SchemaDialog(wx.Dialog):
+    def __init__(self, item, unique_values):
+        wx.Dialog.__init__(self, None, -1, "Schema Setup", size=(200,400))
+        about = wx.StaticText(self, -1, "Please enter information below.")
+        header = wx.StaticText(self, -1, "Header: %s" % item['header'])
+        mapper = wx.StaticText(self, -1, "Mapper:")
+        uvalue = wx.StaticText(self, -1, "Unique values:")
+
+        self.mapper_t = wx.TextCtrl(self, -1, item['mapper'])
+        self.drug_t = wx.CheckBox(self, -1, "Drug")
+        self.show_t = wx.CheckBox(self, -1, "Show")
+        self.values_t = wx.CheckListBox(self, -1, choices=unique_values)
+        ok = wx.Button(self, wx.ID_OK)
+        ok.SetDefault()
+        cancel = wx.Button(self, wx.ID_CANCEL)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(about, 0, wx.EXPAND)
+        sizer.Add(wx.StaticLine(self), 0, wx.EXPAND)
+        fgs = wx.GridBagSizer(vgap=5, hgap=5)
+        fgs.Add(header, (1,1), (1,2), wx.EXPAND)
+        fgs.Add(mapper, (2,1))
+        fgs.Add(self.mapper_t, (2,2))
+        fgs.Add(self.drug_t, (3,1))
+        fgs.Add(self.show_t, (3,2))
+        sizer.Add(fgs, 0, wx.EXPAND)
+        sizer.Add(uvalue)
+        sizer.Add(self.values_t, 0, wx.EXPAND)
+
+        btns = wx.StdDialogButtonSizer()
+        btns.AddButton(ok)
+        btns.AddButton(cancel)
+        btns.Realize()
+        sizer.Add(btns, 0, wx.EXPAND|wx.ALL, 5)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
 
 
 class MainFrame(wx.Frame):
@@ -12,12 +52,12 @@ class MainFrame(wx.Frame):
         helpmenu = wx.Menu()
         filemenu = wx.Menu()
         gridmenu = wx.Menu()
-        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Exit the program")
-        menuAbout = helpmenu.Append(wx.ID_ABOUT, "&About", "About the program")
-        menuImport = gridmenu.Append(wx.ID_OPEN, "&Import", "Import data to grid")
-        menuSumGrid = gridmenu.Append(wx.ID_PREVIEW, "Summarize", "Summarize data")
-        menuEditHeaderGrid = gridmenu.Append(wx.ID_EDIT, "Edit Data Schema", "Edit headers")
-        menuCloseGrid = gridmenu.Append(wx.ID_CLOSE, "Close", "Close the opened grid")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Exit the program.")
+        menuAbout = helpmenu.Append(wx.ID_ABOUT, "&About", "About the program.")
+        menuImport = gridmenu.Append(wx.ID_OPEN, "&Import", "Import data to a grid.")
+        menuSumGrid = gridmenu.Append(wx.ID_PREVIEW, "View Summary", "Summarize data.")
+        menuEditHeaderGrid = gridmenu.Append(wx.ID_EDIT, "Set Schema", "Set a schema for the data.")
+        menuCloseGrid = gridmenu.Append(wx.ID_CLOSE, "Close", "Close the opened grid.")
 
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "&File")
@@ -99,13 +139,14 @@ class MainFrame(wx.Frame):
 
     def OnEditHeaderGrid(self, e):
 
-        columns = ['no.', 'header', 'mapper', 'dtype', 'drug', 'show']
-        headdf = pd.DataFrame(columns=columns)
+        columns = ['header', 'mapper', 'dtype', 'drug', 'keep', 'filter']
+        headdf = {}
 
         def createNewHeaderSchemaList(theList, dataframe, columns):
-            headerdata = []
+            headerdata = defaultdict(list)
             dtypes = dataframe.dtypes
-            for col, text in enumerate(columns):
+            theList.InsertColumn(0, "No.")
+            for col, text in enumerate(columns, start=1):
                 theList.InsertColumn(col, text)
 
             for idx, header in enumerate(dataframe.columns):
@@ -113,44 +154,50 @@ class MainFrame(wx.Frame):
                 theList.SetStringItem(index, 1, header)
                 theList.SetStringItem(index, 2, header)
                 theList.SetStringItem(index, 3, str(dtypes[header]))
-                headerdata.append({
-                    'no.': str(idx+1),
-                    'header': header,
-                    'dtype': str(dtypes[header]),
-                    'mapper': header
-                })
+                headerdata["header"].append(header)
+                headerdata["dtype"].append(str(dtypes[header]))
+                headerdata["mapper"].append(header)
+                headerdata["drug"].append(False)
+                headerdata["keep"].append(True)
+                headerdata["filter"].append({header: []})
 
             theList.SetColumnWidth(1, wx.LIST_AUTOSIZE)
             theList.SetColumnWidth(2, wx.LIST_AUTOSIZE)
             theList.SetColumnWidth(3, wx.LIST_AUTOSIZE)
-            return headerdata
+            return pd.DataFrame(headerdata)
 
         def createHeaderSchemaListFromFile(theList, dataframe, columns):
             theList.ClearAll()
             for col, text in enumerate(columns):
                 theList.InsertColumn(col, text)
 
-            for idx, row in enumerate(dataframe.iterrows()):
-                item = row[1]
+            for idx in range(len(dataframe[columns[0]])):
                 index = theList.InsertStringItem(idx, str(idx+1))
-                for n, colname in enumerate(columns[1:]):
-                    theList.SetStringItem(index, n+1, str(item[colname]))
+                for n, colname in enumerate(columns):
+                    theList.SetStringItem(index, n+1, str(dataframe[colname][idx]))
 
             theList.SetColumnWidth(1, wx.LIST_AUTOSIZE)
             theList.SetColumnWidth(2, wx.LIST_AUTOSIZE)
             theList.SetColumnWidth(3, wx.LIST_AUTOSIZE)
 
+        def OnItemSelected(event, headdf, data):
+            row = int(event.GetItem().GetText())
+            item = headdf.iloc[row-1]
+            unique_values = [u'{}'.format(value) for value in data[item['header']].unique()]
+            dlg = SchemaDialog(item, unique_values)
+            if dlg.ShowModal() == wx.ID_OK:
+                pass
+            dlg.Destroy()
 
-        def OnImport(event, theList, dataframe, columns):
-            openDlg = wx.FileDialog(self, 'Choose a file', self.schemadirname, '', '*.csv;', wx.FD_OPEN)
+
+        def OnLoad(event, theList, dataframe, columns):
+            openDlg = wx.FileDialog(self, 'Choose a file', self.schemadirname, '', '*.json;', wx.FD_OPEN)
             if openDlg.ShowModal() == wx.ID_OK:
                 self.schemafilename = openDlg.GetFilename()
                 self.schemadirname = openDlg.GetDirectory()
                 try:
-                    dataframe.drop(dataframe.index, inplace=True)  # drop previous data
-                    for row in pd.read_csv(
-                        os.path.join(self.schemadirname, self.schemafilename)).iterrows():
-                        dataframe.loc[len(dataframe)] = row[1]
+                    with open(os.path.join(self.schemadirname, self.schemafilename), 'rb') as fp:
+                        dataframe = json.load(fp)
                 except:
                     alertdialog = wx.MessageDialog(self,
                         'An error occurred. Cannot load a schema from the file.')
@@ -163,14 +210,14 @@ class MainFrame(wx.Frame):
                                            'An error occurred. Cannot display a schema.')
                         alertdialog.ShowModal()
                         raise e
-            print(headdf.tail())
 
         def OnSave(event, data):
             if not self.schemafilename:  # the schema has never been saved before
                 OnSaveAs(event, data)
             else:
                 try:
-                    data.to_csv(os.path.join(self.schemadirname, self.schemafilename), index=False)
+                    with open(os.path.join(self.schemadirname, self.schemafilename), 'w') as fp:
+                        json.dump(data, fp, indent=True)
                 except:
                     alertdialog = wx.MessageDialog(self,
                                         'An error occurred. Cannot save a schema to disk.')
@@ -180,36 +227,40 @@ class MainFrame(wx.Frame):
 
         def OnSaveAs(event, data):
             saveDlg = wx.FileDialog(self, 'Choose a file',
-                                        self.schemadirname, '', '*.csv;', wx.FD_SAVE)
+                                        self.schemadirname, '', '*.json;', wx.FD_SAVE)
             if saveDlg.ShowModal() == wx.ID_OK:
                 self.schemafilename = saveDlg.GetFilename()
                 self.schemadirname = saveDlg.GetDirectory()
                 try:
-                    data.to_csv(os.path.join(self.schemadirname, self.schemafilename), index=False)
-                except:
+                    with open(os.path.join(self.schemadirname, self.schemafilename), 'w') as fp:
+                        json.dump(data, fp, indent=True)
+                except Exception as e:
                     alertdialog = wx.MessageDialog(self, 'An error occurred. Cannot save a schema to disk.')
                     alertdialog.ShowModal()
             saveDlg.Destroy()
 
         fileMenu = wx.Menu()
-        menuImport = fileMenu.Append(wx.NewId(), "&Import Schema", "Import schema")
-        menuSave = fileMenu.Append(wx.NewId(), "&Save Schema", "Save schema")
-        menuSaveAs = fileMenu.Append(wx.NewId(), "S&ave Schema As..", "Save schema as..")
-        menuExit = fileMenu.Append(wx.NewId(), "&Close Window", "Close")
+        menuLoad = fileMenu.Append(wx.NewId(), "&Load Schema", "Load a schema from a file.")
+        menuApply = fileMenu.Append(wx.NewId(), "&Apply Schema", "Apply this schema to this dataset.")
+        menuSave = fileMenu.Append(wx.NewId(), "&Save Schema", "Save this schema to a file.")
+        menuSaveAs = fileMenu.Append(wx.NewId(), "S&ave Schema As..",
+                                        "Save this schema a new file.")
+        menuExit = fileMenu.Append(wx.NewId(), "&Close Window", "Close the window.")
         menuBar = wx.MenuBar()
-        menuBar.Append(fileMenu, "&Menu")
+        menuBar.Append(fileMenu, "&Action")
 
-        dlg = wx.Frame(self, title='Edit Schema')
+        dlg = wx.Frame(self, title='Schema Manager')
         dlg.CreateStatusBar(1)
         dlg.SetMenuBar(menuBar)
         dlg.Bind(wx.EVT_MENU, OnClose, menuExit)
         dlg.Bind(wx.EVT_MENU, lambda e: OnSaveAs(e, headdf), menuSaveAs)
 
         theList = wx.ListCtrl(dlg, -1, style=wx.LC_REPORT|wx.LC_HRULES|wx.LC_VRULES)
-        dlg.Bind(wx.EVT_MENU, lambda e: OnImport(e, theList, headdf, columns), menuImport)
+        dlg.Bind(wx.EVT_MENU, lambda e: OnLoad(e, theList, headdf, columns), menuLoad)
         if not self.schemafilename:  # new schema
-            headdf = headdf.append(createNewHeaderSchemaList(theList, self.df, columns))
+            headdf = createNewHeaderSchemaList(theList, self.df, columns)
         dlg.Show()
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, lambda e: OnItemSelected(e, headdf, self.df), theList)
 
 
 if __name__ == '__main__':
